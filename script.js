f6aec145bb75119084e04a38ff0bb9b48ea46af4
William-04 <William177013@gmail.com>
2022-01-01 22:08:28 -0500

    Homescreen Changes
    - Loads locked page with unfinished preview of games

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const aboutContainer = document.getElementById('aboutContainer')
 const buttons = document.getElementsByClassName("button")
 const aboutSection = document.getElementById("aboutSection")
 const aboutBtn = document.getElementById("aboutBtn")
+const lockBtn = document.getElementById("lockBtn")
+const playBtn = document.getElementById("playBtn")
 const sec1 = document.getElementById('sec1')
 const header = document.getElementById("header")
 const closeAbout = document.getElementById("closeAbout")
 const aboutText = document.getElementById("aboutText")
+const lockedSection = document.getElementById("lockedSection")
+const closeLocked = document.getElementById("closeLocked")
 var buttonSound1 = document.getElementById('audio1')
 var buttonSound2 = document.getElementById('audio2')
 //https://www.epidemicsound.com/music/featured/
@@ -42,29 +46,38 @@ aboutBtn.addEventListener("click", ()=> {
   hideDiv(sec1)
   hideDiv(header)
   showDiv(aboutSection)
-  showDiv(aboutContainer)
-  showDiv(aboutText)
-  showDiv(closeAbout)
   console.log("opened")
 }) 
 
 // Manipulating the DOM if the X button is pressed on the about section 
 closeAbout.addEventListener("click", ()=> { 
   playSound2()
-  hideDiv(closeAbout)
-  hideDiv(aboutText)
-  hideDiv(aboutContainer)
   hideDiv(aboutSection)
   showDiv(sec1)
   showDiv(header)
   console.log('closed')
 })
 
+// ================== LOCKED BUTTON PAGE ===============================
 
+lockBtn.addEventListener("click", () => {
+  hideDiv(sec1)
+  hideDiv(header)
+  showDiv(lockedSection)
+  //const container = document.createElement('div')
+  //const preview = document.createElement('div')
+  //container.appendChild(preview)
+  //container.setAttribute('id', 'lockedContainer')
+  //preview.classList.add("previewBtn")
 
-//Option 1  - Different functions to make new scereens on same javascript file 
-
-//Option 2 - Different Javascript file, using HTML to manipulate the different links 
+  })
+closeLocked.addEventListener("click", ()=> { 
+    playSound2()
+    hideDiv(lockedSection)
+    showDiv(sec1)
+    showDiv(header)
+    console.log('closed')
+  })
 
 
 
